ce43f8465e3b211cae64637439d248db0070c3fa
MrLadino <andresladino0324@gmail.com>
2025-04-02 11:15:54 -0500

    actualizacion de ruta de local a despliegue

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// Usamos HashRouter en lugar de BrowserRouter
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./Context/AuthContext";
 import App from "./App";
 import "./index.css";
@@ -10,9 +9,9 @@ import "./index.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </AuthProvider>
   </StrictMode>
 );
